ec9c9d77f2140943bc37e403249eadf7063a8ced
Hałas Piotr <halaspiotr@gmail.com>
2025-08-13 23:26:43 +0200

    Reorganize imports in MicroApp for better readability

--- src/MicroApp.php
+++ src/MicroApp.php
@@ -8,13 +8,13 @@ use CrazyGoat\MicroApp\Attributes\Route;
 use FastRoute\Dispatcher;
 use FastRoute\RouteCollector;
 
-use Symfony\Component\Console\Input\InputArgument;
 use function FastRoute\simpleDispatcher;
 
 use ReflectionClass;
 use Symfony\Component\Console\Application;
 use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
